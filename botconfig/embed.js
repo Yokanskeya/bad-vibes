985c97ecdec5b264d7280aae7424cc49633223c3
adamkaisa <75810467+adamkhai08@users.noreply.github.com>
2022-02-14 05:18:44 +0700

    Update embed.js

--- botconfig/embed.js
+++ botconfig/embed.js
@@ -3,7 +3,7 @@ module.exports= {
     succescolor: "GREEN",
     wrongcolor: "#e01e01",
     ERROR: "es.thumb ? url.img.ERROR : null",
-    footertext: `Bad Vibes • Version ${require('../package.json').version}`,
+    footertext: `© Bad Vibes Bot`,
     footericon: "https://cdn.discordapp.com/attachments/784917578974756904/939312438849986560/New_BV_icon.png?size=96",
 };
 
