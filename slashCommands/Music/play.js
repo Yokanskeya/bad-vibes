87d07e28e729b6a1e1f6a02ecb491e464e791736
adamkaisa <75810467+adamkhai08@users.noreply.github.com>
2022-02-14 13:29:40 +0700

    Update play.js

--- slashCommands/Music/play.js
+++ slashCommands/Music/play.js
@@ -35,7 +35,7 @@ const { handlemsg } = require(`${process.cwd()}/handlers/functions`);
       let args = [interaction.options.getString("what_song")]
       if(!args[0]) args = [interaction.options.getString("song")]
       //Send information
-      interaction.reply({content: `Searching and attempting to play: **${args[0]}** from <:Youtube:840260133686870036> \`Youtube\`!`})
+      interaction.reply({content: `Searching and attempting to play: **${args[0]}** from 🎵 \`Youtube\`!`})
       //play the SONG from YOUTUBE
       playermanager(client, message, args, `song:youtube`, interaction);
     } catch (e) {
